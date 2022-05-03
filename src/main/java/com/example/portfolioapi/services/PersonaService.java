cdc929ea6f021ac9edec38c929564321477e4227
waltermolina <waltermolina@msn.com>
2022-05-03 18:51:25 -0300

    Agregado servicio para persona

---
+++ src/main/java/com/example/portfolioapi/services/PersonaService.java
@@ -0,0 +1,36 @@
+package com.example.portfolioapi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.tareasapi.models.PersonaModel;
+import com.example.tareasapi.repos.PersonaRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService {
+    @Autowired
+    PersonaRepo personaRepo;
+
+    public List<PersonaModel> getAll(){
+        return (List<PersonaModel>) personaRepo.findAll();
+    }
+
+    public ResponseEntity<PersonaModel> getById(int id) {
+
+        Optional<PersonaModel> persona = personaRepo.findById(id);
+
+        if (persona.isPresent()) {
+            return ResponseEntity.ok().body(persona.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public PersonaModel save(PersonaModel persona) {
+        return personaRepo.save(persona); // crear o actualizar
+    }
+}

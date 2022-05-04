395c0c3b6459539d649093843c6e13b8904ffb0c
Nicolas <nicogamarra2010@gmail.com>
2022-05-04 12:56:03 -0300

    modificado atributos de ProyectoModel

--- src/main/java/com/example/portfolioapi/models/ProyectoModel.java
+++ src/main/java/com/example/portfolioapi/models/ProyectoModel.java
@@ -16,56 +16,83 @@ public class ProyectoModel {
     //@Column(unique = true, nullable = false)
     public int idproyecto;
     
-    @Basic
-    private String proyecto;
-    private String descrip;
+    //@Basic
+    private String titulo;
+    private String descripcion;
+    
     private String url;
+    private int persona;
     
-    public ProyectoModel(int idproyecto, String descrip, String url) {
-        this.idproyecto = idproyecto;
-        this.descrip = descrip;
-        this.url = url;
+   
+
+    public ProyectoModel() {
     }
-    
+
+    public ProyectoModel(int idproyecto, String titulo, String descripcion, String url, int persona) {
+        this.idproyecto = idproyecto;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.url = url;
+        this.persona = persona;
+    }
+
+    public ProyectoModel(String titulo, String descripcion, String url, int persona) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.url = url;
+        this.persona = persona;
+    }
+
     public int getIdproyecto() {
         return idproyecto;
     }
-    
+
     public void setIdproyecto(int idproyecto) {
         this.idproyecto = idproyecto;
     }
-    
-    public String getProyecto() {
-        return proyecto;
+
+    public String getTitulo() {
+        return titulo;
     }
-    
-    public void setProyecto(String proyecto) {
-        this.proyecto = proyecto;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
-    
-    public String getDescrip() {
-        return descrip;
+
+    public String getDescripcion() {
+        return descripcion;
     }
-    
-    public void setDescrip(String descrip) {
-        this.descrip = descrip;
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
-    
+
     public String getUrl() {
         return url;
     }
-    
+
     public void setUrl(String url) {
         this.url = url;
     }
-    
-    
-    
-    
+
+    public int getPersona() {
+        return persona;
+    }
+
+    public void setPersona(int persona) {
+        this.persona = persona;
+    }
+
     @Override
     public String toString() {
-        return "ProyectoModel{" + "idproyecto=" + idproyecto + ", description=" + descrip + ", url=" + url + '}';
+        return "ProyectoModel{" + "idproyecto=" + idproyecto + ", titulo=" + titulo + ", descripcion=" + descripcion + ", url=" + url + ", persona=" + persona + '}';
     }
+    
+    
+    
+    
+    
+   
     
 }
 

622bc46387a23dfb6cf35acd1ab53a6c2202faf5
fernar75 <94242277+fernar75@users.noreply.github.com>
2022-05-04 21:58:30 -0300

    agregar Cors

--- src/main/java/com/example/portfolioapi/controllers/SkillController.java
+++ src/main/java/com/example/portfolioapi/controllers/SkillController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/skills")
 public class SkillController {
